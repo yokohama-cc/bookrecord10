397cfe158f32303c706cfcafd38f8f5f78fa186e
yokohama-cc <bummysayakakenta@gmail.com>
2023-05-24 11:09:48 +0900

    commit 20230524

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/',[AssignedBookController::class,'index']);
+    Route::get('/dashboard',[AssignedBookController::class,'index'])->name('dashboard');
     /*Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');*/
